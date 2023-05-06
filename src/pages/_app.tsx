118682c9c240a2c45e96f8c9704f40eb40836f35
chiragthapa777 <chiragthapa777@gmail.com>
2023-05-06 19:41:07 +0545

    add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,13 @@ import Layout from '@/components/Layout';
 // import { WebsocketProvider, socket } from '@/contexts/WebsocketContext';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 
 export default function App({ Component, pageProps }: AppProps) {
+	useEffect(()=>{
+		console.log("hittttt")
+	},[])
   return (
 		<Layout>
 			{/* <WebsocketProvider value={socket}> */}
